b060c21e2a712056ccd96d68f2fdc36d4cf25a0d
minchih <cooker0521@gmail.com>
2024-09-21 09:26:13 +0800

    20240921

--- scrape.py
+++ scrape.py
@@ -5,6 +5,13 @@ import pandas as pd
 url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON"
 
 
+def get_six_pm25_json():
+    columns, values = scrape_six_pm25()
+
+    json_data = {"site": columns, "pm25": values}
+    return json_data
+
+
 def get_pm25_json():
     columns, values = scrape_pm25()
     xdata = [value[0] for value in values]
@@ -13,11 +20,46 @@ def get_pm25_json():
     return json_data
 
 
+def convert_value(value):
+    try:
+        return eval(value)
+    except:
+        return None
+
+
+def get_pm25_data():
+    datas = datas = requests.get(url).json()["records"]
+    df = pd.DataFrame(datas)
+    # 將非數值轉成None
+    df["pm25"] = df["pm25"].apply(convert_value)
+    # 用dataframe的dropna去除含None的數據
+    df = df.dropna()
+    return df
+
+
+def scrape_six_pm25():
+    six_countys = ["臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市"]
+    pm25 = []
+    try:
+        df = get_pm25_data()
+        for county in six_countys:
+            avg_pm25 = df.groupby("county").get_group(county)["pm25"].mean()
+            pm25.append(round(avg_pm25, 2))
+            # print(county, avg_pm25)
+
+        columns = six_countys
+        values = pm25
+
+        return columns, values
+    except Exception as e:
+        print(e)
+
+    return None, 404
+
+
 def scrape_pm25(sort=False, ascending=True):
     try:
-        datas = datas = requests.get(url).json()["records"]
-        df = pd.DataFrame(datas)
-        df["pm25"] = df["pm25"].apply(lambda x: eval(x))
+        df = get_pm25_data()
         if sort:
             df = df.sort_values("pm25", ascending=ascending)
 
@@ -58,4 +100,4 @@ def scrape_stocks():
 if __name__ == "__main__":
     # print(scrape_stocks())
     # print(scrape_pm25(sort=True, ascending=True))
-    print(get_pm25_json())
+    print(scrape_six_pm25())
